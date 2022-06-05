925240a76f054b4e16d54c39770e462bd5a4ff4f
Eugene <Jiztastamablastamarang@gmail.com>
2022-06-05 18:32:27 +0800

    Read method

--- main.go
+++ main.go
@@ -16,20 +16,10 @@ func main() {
 	io.Copy(os.Stdout, &r)
 }
 
-func deCipher(char byte, register byte) (byte, byte) {
-	if register == 0 {
-		return char, 0
+func (rot rot13Reader) Read(char []byte) (int, error) {
+	n, err := rot.r.Read(char)
+	for i := 0; i < n; i++ {
+		char[i], _ = deCipher(isAlpha(char[i]))
 	}
-	return register + (char-register+13)%26, 1
-
-}
-
-func isAlpha(char byte) (byte, byte) {
-	if char >= 'A' && char <= 'Z' {
-		return char, 'A'
-	} else if char >= 'a' && char <= 'z' {
-		return char, 'a'
-	} else {
-		return char, 0
-	}
+	return n, err
 }
